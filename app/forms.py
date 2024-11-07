805eae142c9e75fa05f55eb6ac4467d6df589791
melhaj7 <ichibakry7@gmail.com>
2024-11-07 18:57:34 +0100

    finish form and add initial setup for sqlite

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,6 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
+from flask_wtf import FlaskForm # pylint: disable=import-error
+from wtforms import StringField, PasswordField, BooleanField, SubmitField # pylint: disable=import-error
+from wtforms.validators import DataRequired # pylint: disable=import-error
 
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
